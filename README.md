a80e117a0a03945c9f660020623c16d9047f0f4d
Alicia Sykes <sykes.alicia@gmail.com>
2019-03-30 15:58:11 +0000

    Quick homepage for a domain

---
+++ README.md
@@ -0,0 +1,7 @@
+## Awesome Code, Homepage
+
+Not much to see here.... Just made a super quick homepage for my company domain, so that it's not blank.
+
+Live site running at: [https://awesome-code.co.uk](https://awesome-code.co.uk)
+
+![Nothing much to see here...](banner.gif "Screenshot")
---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Vanta cool animation
+VANTA.WAVES({
+    el: "#the-cool-background",
+    color: 0x20658c,
+    waveHeight: 25.00,
+    waveSpeed: 0.45,
+    zoom: 0.87
+});
+
+// Google Analytics, evil but necissary tracking
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-53932003-11');
